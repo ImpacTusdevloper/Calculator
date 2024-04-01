a61349e9a187b1c911e9f7d92bdf4b9857a79d60
ImpacTusdevloper <krishkushwaha99999@gmail.com>
2024-04-01 15:28:13 +0530

    Initial

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SupportLib;
+
+//Getting Input
+Support.Separator();
+string? inp = Support.Input("Enter equation: ");
+
--- SupportLib/Support.cs
+++ SupportLib/Support.cs
@@ -1,16 +1,25 @@
 ﻿namespace SupportLib;
 
-public class Support
+public static class Support
 {
-    public void Separator(ConsoleColor color, char sep = '-')
+    public static void Separator(ConsoleColor color = ConsoleColor.Red, char sep = '-')
     {
         Console.ForegroundColor = color;
-
+        Console.Write("\n \n");
         int initialTopPos = Console.CursorTop;
         int width = Console.WindowWidth;
-        while(Console.CursorLeft <= width && Console.CursorTop == initialTopPos)
-        {
+        while(Console.CursorLeft < width && Console.CursorTop == initialTopPos)
+            Console.Write(sep);
+        
+        Console.Write("\n \n");
+        Console.ResetColor();
+    }
 
-        }
+    public static string? Input(string arg = "")
+    {
+        if(arg != "")
+            Console.Write(arg);
+        string? inp = Console.ReadLine();
+        return inp;
     }
 }
