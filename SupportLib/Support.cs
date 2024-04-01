f84d19f0e65b4895de3b334ff96d64cb09a286e6
ImpacTusdevloper <krishkushwaha99999@gmail.com>
2024-04-01 15:27:45 +0530

    Initial

---
+++ SupportLib/Support.cs
@@ -0,0 +1,16 @@
+﻿namespace SupportLib;
+
+public class Support
+{
+    public void Separator(ConsoleColor color, char sep = '-')
+    {
+        Console.ForegroundColor = color;
+
+        int initialTopPos = Console.CursorTop;
+        int width = Console.WindowWidth;
+        while(Console.CursorLeft <= width && Console.CursorTop == initialTopPos)
+        {
+
+        }
+    }
+}
